67c7fd2d18d665b84d416626ab100582505b2263
r04945027 <r04945027@linux7.csie.ntu.edu.tw>
2016-10-17 14:51:57 +0800

    update main script of hw1

--- hw1/src/hw1.m
+++ hw1/src/hw1.m
@@ -1,7 +1,7 @@
 function hw1()
     close all
     tspan = [0 5];
-    y0 = [1; 1; 1/2; 0; 0; 0;];
+    y0 = [ 1; 1; 1/2; 0; 0; 0;];
     [t,y] = ode45(@ODE1,tspan,y0);
     figure;
     plot(t, y(:,1), '-s', t, y(:,2), '-+', t, y(:,3), '-.', t, y(:,4), '-*', t, y(:,5), '-o',t,y(:,6), '-x');
@@ -9,6 +9,15 @@ function hw1()
     ylabel('Concentration (mM)');
     legend('A','B','C','D','E','F');
     figure;
+    y0 = [ 1; 1; 1/2; 0; 0; 0;];
+    [t,y] = ode45(@ODE1,tspan,y0);
+    figure;
+    plot(t, y(:,1), '-s', t, y(:,2), '-+', t, y(:,3), '-.', t, y(:,4), '-*', t, y(:,5), '-o',t,y(:,6), '-x');
+    xlabel('Time (sec)');
+    ylabel('Concentration (mM)');
+    legend('A','B','C','D','E','F');
+    figure;
+    y0 = [ 1.5; 3; 2;];
 end
 
 function dydt = ODE1(t,y) % question 2.4.7 a) iii)
@@ -49,4 +58,28 @@ function dydt = ODE2(t,y) % question 2.4.7 b) iii)
     dydt = dydt';
 end
 
+function dydt = ODE3(t,y) % question 2.4.8 a)
+    k1 = 0.05;
+    k2 = 0.7;
+    km1 = 0.005;
+    km2 = 0.4;
+    dydt(1) =-k1 * y(1) + km1 * y(2); % dA/dt
+    dydt(2) = k1 * y(1) - km1 * y(2) - k2 * y(2) + km2 * y(3); % dB/dt
+    dydt(3) = k2 * y(2) - km2 * y(3); % dC/dt
+    dydt = dydt';
+end
 
+function dydt = ODE4(t,y) % question 2.4.8 c)
+    
+end
+
+function dydt = ODE5(t,y) % question 2.4.9 a)
+    k0 = 1;
+    k1 = 11;
+    km1 = 8;
+    k2 = 0.2;
+    dydt(1) = k0 - k2 * y(1) - k1 * y(1) + km1 * y(2); % dA/dt
+    dydt(2) = k1 * y(1) - km1 * y(2); % dB/dt
+end
+
+function dydt = ODE6(t,y) % question 2.4.9 c)
